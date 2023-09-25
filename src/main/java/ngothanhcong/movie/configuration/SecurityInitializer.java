14e12a50a33a6f956ef9e83b57eac77b46dc6e68
ngothanhcong111 <congdu1102@gmail.com>
2023-09-25 16:48:00 +0700

    First Commit

--- src/main/java/ngothanhcong/movie/configuration/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package ngothanhcong.movie.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
